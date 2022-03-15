8122e0eff8674f7a95530d1755c75a2f25623fa0
Wilfex <wilfex81@gmail.com>
2022-03-15 03:29:53 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "holberton.h"
 
 /**
-*putchar to print the characters given
-* main putchar
-*char c the charcters given
-* Return: Always 0 (Success)
-*/
-int putchar(char c)
-{
-        write(1, &c, 8);
-        return(0);
-}
-
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        putchar("_putchar\n");
-        return (0);
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
