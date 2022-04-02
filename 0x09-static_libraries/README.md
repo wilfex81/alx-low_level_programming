eb80b6a0acdbf15f7421d839ea7ed359a6d0e24f
wilfex <wilfex81@gmail.com>
2022-04-02 22:11:24 +0300

    0x09-static_libraries

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,1 @@
-libmy.a containing all the functions create_static_lib.sh creates a static library called liball.a from all the .c files that are in the current directory. main.h Header file that contains prototypes of the functions used.
+create_static_lib.sh script creates a static library called liball.a from all the .c files that are in the current directory
--- 0x09-static_libraries/functions/2-strncpy.c
+++ 0x09-static_libraries/functions/2-strncpy.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
  * _strncpy - copies a string
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes from src.
- * Return: the pointer to dest.
+ * @dest: destination str
+ * @src: source str
+ * @n: number of bytes(chars) to copy
+ * Return: pointer to the resulting str
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for ( ; i < n; i++)
+
+	for (; i < n; i++)	/* same as -> while (i < n)... i++ */
 		dest[i] = '\0';
 
 	return (dest);
