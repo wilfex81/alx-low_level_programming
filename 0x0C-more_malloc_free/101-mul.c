c055c80c39425e4793d2ee927ea5821489450ad2
wilfex <wilfex81@gmail.com>
2022-04-07 16:54:21 +0300

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,19 +14,21 @@ void _is_zero(char *argv[])
 	int i, isn1 = 1, isn2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
+	{
 		if (argv[1][i] != '0')
 		{
 			isn1 = 0;
 			break;
 		}
-
+	}
 	for (i = 0; argv[2][i]; i++)
+	{
 		if (argv[2][i] != '0')
 		{
 			isn2 = 0;
 			break;
 		}
-
+	}
 	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
@@ -46,8 +48,10 @@ char *_initialize_array(char *ar, int lar)
 	int i = 0;
 
 	for (i = 0; i < lar; i++)
+	{
 		ar[i] = '0';
 	ar[lar] = '\0';
+	}
 	return (ar);
 }
 
@@ -64,12 +68,13 @@ int _checknum(char *argv[], int n)
 	int ln;
 
 	for (ln = 0; argv[n][ln]; ln++)
+	{
 		if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
-
+	}
 	return (ln);
 }
 
@@ -124,7 +129,6 @@ int main(int argc, char *argv[])
 	printf("%s\n", nout);
 	return (0);
 }
-
 
 
 
